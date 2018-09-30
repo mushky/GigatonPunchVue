d869db92ae0b1b28afc1338a7a880a32149bb504
Ali Zaidi <msazaidi@gmail.com>
2018-09-29 20:37:24 -0700

    replay section to container-fluid test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ axios.get('https://www.googleapis.com/youtube/v3/playlistItems', {
   params: {
     part: 'snippet',
     playlistId: 'PLnw0oJfCMWaR10TuwTXmLVUQS6CPvOCd2', // good id
-    maxResults: 9, 
+    maxResults: 6, 
     key: 'AIzaSyBg0oosTPGdnxUkoB2Y0nlg4fAPA7MbF0U'
   }
 })
